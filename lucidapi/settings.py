75a168e7f934d4cb4eb16cae86c149dad7ae45c2
nathaniel nosa <nathanielnarth@gmail.com>
2024-11-01 12:53:06 +0100

    database settings

--- lucidapi/settings.py
+++ lucidapi/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,13 +70,17 @@ WSGI_APPLICATION = 'lucidapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default':dj_database_url.parse(os.environ['DATABASE_URL'])
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
